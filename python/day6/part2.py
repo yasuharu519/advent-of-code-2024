c1de70c3a47e444faaee32f80e1c4245d53a6149
Yasuharu Sawada <yasuharu519@gmail.com>
2024-12-27 23:03:53 +0900

    day6 の処理を改善

--- python/day6/part2.py
+++ python/day6/part2.py
@@ -1,43 +1,48 @@
 # coding: utf-8
 import sys
 
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+def get_start_coord(lines):
+    for i, line in enumerate(lines):
+        for j, c in enumerate(line):
+            if c == "^":
+                return (i, j)
+    return -1, -1
+
+def detect_loop(x, y, d, lines, passed) -> bool:
+    m = len(passed)
+    n = len(passed[0])
+    local_passed = [[0] * n for _ in range(m)]
+
+    while 0 <= x < m and 0 <= y < n:
+        if local_passed[x][y] & (1 << d) or passed[x][y] & (1 << d):
+            return True
+        local_passed[x][y] |= (1 << d | passed[x][y])
+        dx, dy = directions[d]
+        nx, ny = x + dx, y + dy
+        while 0 <= nx < m and 0 <= ny < n and lines[nx][ny] == "#":
+            d = (d + 1) % 4
+            dx, dy = directions[d]
+            nx, ny = x + dx, y + dy
+        x, y = nx, ny
+    return False
+
 def main():
     lines = [list(x.strip()) for x in sys.stdin.readlines()]
     m = len(lines)
     n = len(lines[0])
     
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     d = 0
+    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    x, y = get_start_coord(lines)
+    passed = [[0] * n for _ in range(m)]
 
-    x, y = 0, 0
-    for i, line in enumerate(lines):
-        for j, c in enumerate(line):
-            if c == "^":
-                x, y = (i, j)
-                break
-    sx, sy = x, y
-
-    def detect_loop(x, y, d) -> bool:
-        flags = [[0] * n for _ in range(m)]
-
-        while 0 <= x < m and 0 <= y < n:
-            if (flags[x][y] & (1 << d)) != 0:
-                return True
-            flags[x][y] |= 1 << d
-            dx, dy = directions[d]
-            nx, ny = x + dx, y + dy
-            while 0 <= nx < m and 0 <= ny < n and lines[nx][ny] == "#":
-                d = (d + 1) % 4
-                dx, dy = directions[d]
-                nx, ny = x + dx, y + dy
-            x, y = nx, ny
-        return False
+    obstacles = 0
 
     # 候補を出しておく
-    candidates = set()
     while 0 <= x < m and 0 <= y < n:
-        if lines[x][y] == ".":
-            candidates.add((x, y))
+        passed[x][y] |= 1 << d
         dx, dy = directions[d]
         nx, ny = x + dx, y + dy
 
@@ -46,21 +51,17 @@ def main():
                 d = (d + 1) % 4
                 dx, dy = directions[d]
                 nx, ny = x + dx, y + dy
+            lines[nx][ny] = "#"
+            
+            # 障害物を配置したときにループになるか試す
+            if passed[nx][ny] == 0 and detect_loop(x, y, (d + 1) % 4, lines, passed):
+                obstacles += 1
+            lines[nx][ny] = "."
             x, y = nx, ny
         else:
             break
     
-    print("candidates num: ", len(candidates))
-    
-    # 各候補ごとに絞り込み
-    result = 0
-    for ox, oy in list(candidates):
-        lines[ox][oy] = "#"
-        if detect_loop(sx, sy, 0):
-            result += 1
-        lines[ox][oy] = "."
-
-    print(result)
+    print(obstacles)
 
 if __name__ == "__main__":
     main()
