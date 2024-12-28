600961d0b79c891152ab059e50ed78b0836a974b
Yasuharu Sawada <yasuharu519@gmail.com>
2024-12-28 23:15:16 +0900

    refactor

--- python/day9/part2.py
+++ python/day9/part2.py
@@ -4,49 +4,36 @@ from collections import deque
 def main():
     line = input().strip()
 
-    actualIndex = 0
-    nums = deque()
+    files = deque()
     spaces = []
+    pos = 0
     for i,c in enumerate(line):
         num = int(c)
-        # num, index
         if i % 2 == 0:
-            # file
-            nums.append((num, actualIndex, i // 2))
+            files.append((num, pos, i // 2))
         else:
-            spaces.append((num, actualIndex, i // 2))
-        actualIndex += int(c)
+            spaces.append((num, pos, i // 2))
+        pos += int(c)
 
     result = 0
-    while nums:
-        num, index, file_index = nums.pop()
-        # print(f"num: {num}, index: {index}, file_index: {file_index}")
-        # print(f"space: {spaces}")
-        # num 以上の space を探す
-
-        # space_index = bisect.bisect_left(spaces, (num, -1, 0))
-        # while space_index < len(spaces) and spaces[space_index][1] >= index:
-        #     spaces.pop(space_index)
-
+    while files:
+        num, index, pos = files.pop()
         space_index = 0
         while space_index < len(spaces):
             space = spaces[space_index]
             if space[0] >= num and space[1] <= index:
                 spaces.pop(space_index)
                 start, end = space[1], space[1] + num - 1
-                result += file_index * (start + end) * num // 2
+                result += pos * (start + end) * num // 2
 
                 if space[0] > num:
                     spaces.insert(space_index, (space[0] - num, space[1] + num, space[2]))
                 break
             space_index += 1
-
-        # # 見つからなかった場合
-        if space_index == len(spaces):
+        else:
             start, end = index, index + num - 1
-            result += file_index * (start + end) * num // 2
-            print(f"file_index: {file_index}, start: {start}, end: {end}, result: {result}")
-        # else:
+            result += pos * (start + end) * num // 2
+            print(f"pos: {pos}, start: {start}, end: {end}, result: {result}")
     print(result)
 
 
