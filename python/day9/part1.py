86f0902c10b3ad35daf45bbf59c4c79c7350973f
Yasuharu Sawada <yasuharu519@gmail.com>
2024-12-28 20:16:34 +0900

    refactor

--- python/day9/part1.py
+++ python/day9/part1.py
@@ -5,49 +5,50 @@ def main():
     line = input().strip()
 
     queue = deque()
-    actualIndex = 0
-    for i,c in enumerate(line):
-        print(i, c)
-        # num, index
+    pos = 0
+    for i, c in enumerate(line):
+        c = int(c)
         if i % 2 == 0:
-            # file
-            queue.append((int(c), i // 2, actualIndex, True))
+            # isNum, num, file_index, pos
+            queue.append((True, c, pos, i // 2))
         else:
-            queue.append((int(c), i // 2, actualIndex, False))
-        actualIndex += int(c)
+            queue.append((False, c, pos))
+        pos += int(c)
 
     if queue[-1][2] == False:
         queue.pop()
 
     result = 0
     while queue:
-        num, file_index, index, isNum = queue.popleft()
+        file_data = queue.popleft()
+        is_num, rest = file_data[0], file_data[1:]
         # 数字の場合
-        if isNum:
-            start, end = index, index + num - 1
+        if is_num:
+            num, pos, file_index = rest
+            start, end = pos, pos + num - 1
             result += file_index * (start + end) * num // 2
             print(f"file_index: {file_index}, start: {start}, end: {end}, result: {result}")
         # space の場合
         else:
-            rest = num
-            while queue and rest > 0:
-                while queue[-1][3] == False:
-                    queue.pop()
-                if not queue:
-                    break
-                lastNum, lastFileIndex, lastIndex, _ = queue.pop()
-                if rest >= lastNum:
-                    start, end = index, index + lastNum - 1
-                    result += lastFileIndex * (start + end) * lastNum // 2
-                    rest -= lastNum
-                    index += lastNum
-                    print(f"file_index: {lastFileIndex}, start: {start}, end: {end}, result: {result}")
-                else:
-                    start, end = index, index + rest - 1
-                    result += lastFileIndex * (start + end) * rest // 2
-                    queue.append((lastNum - rest, lastFileIndex, lastIndex, True))
-                    rest = 0
-                    print(f"file_index: {lastFileIndex}, start: {start}, end: {end}, result: {result}")
+            num, pos = rest
+            # 末尾が space の場合は削除
+            while queue and queue[-1][0] == False:
+                queue.pop()
+            if not queue:
+                break
+            # 末尾から数字のものを取り出す
+            _, last_num, _last_pos, file_index = queue.pop()
+            if num >= last_num:
+                taken_num = last_num
+                # スペースを先頭に追加
+                queue.appendleft((False, num - taken_num, pos + taken_num))
+            else:
+                taken_num = num
+                # 残りの数を末尾に追加
+                queue.append((True, last_num - taken_num, pos + taken_num, file_index))
+            start, end = pos, pos + taken_num - 1
+            result += file_index * (start + end) * taken_num // 2
+            print(f"file_index: {file_index}, start: {start}, end: {end}, result: {result}")
     print(result)
 
 if __name__ == "__main__":
