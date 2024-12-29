c91c4e4a401905a55431078ac06c3db4784d234c
Yasuharu Sawada <yasuharu519@gmail.com>
2024-12-29 15:24:20 +0900

    Refactor day10

--- python/day10/part1.py
+++ python/day10/part1.py
@@ -1,35 +1,33 @@
-# coding: utf-8
 import sys
+from collections import deque
 from functools import cache
+
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 def main():
     lines = [list(map(int, list(line.strip()))) for line in sys.stdin.readlines()]
-    print(lines)
     m = len(lines)
     n = len(lines[0])
 
-    @cache
-    def dfs(x, y) -> int:
-        v = int(lines[x][y])
-        if v == 9:
-            return 1
-        
-        result = 0
-        for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < m and 0 <= ny < n:
-                if lines[nx][ny] == v + 1:
-                    result += dfs(nx, ny)
-        return result
+    def count(x, y) -> set:
+        queue = deque([(0, x, y)])
+        reached = set()
+        while queue:
+            c, xx, yy = queue.popleft()
+            if c == 9:
+                reached.add((xx, yy))
+                continue
+            for dx, dy in directions:
+                nx, ny = xx + dx, yy + dy
+                if 0 <= nx < m and 0 <= ny < n and lines[nx][ny] == c + 1:
+                    queue.append((c + 1, nx, ny))
+        return len(reached)
     
     result = 0
     for i in range(m):
         for j in range(n):
             if lines[i][j] == 0:
-                res = dfs(i, j)
-                print(i, j, res)
-                result += res
-
+                result += count(i, j)
     print(result)
 
 
--- python/day10/part2.py
+++ python/day10/part2.py
@@ -1,10 +1,10 @@
-# coding: utf-8
 import sys
 from functools import cache
 
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
 def main():
     lines = [list(map(int, list(line.strip()))) for line in sys.stdin.readlines()]
-    print(lines)
     m = len(lines)
     n = len(lines[0])
 
@@ -13,23 +13,18 @@ def main():
         v = int(lines[x][y])
         if v == 9:
             return 1
-        
         result = 0
-        for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        for dx, dy in directions:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < m and 0 <= ny < n:
-                if lines[nx][ny] == v + 1:
-                    result += dfs(nx, ny)
+            if 0 <= nx < m and 0 <= ny < n and lines[nx][ny] == v + 1:
+                result += dfs(nx, ny)
         return result
     
     result = 0
     for i in range(m):
         for j in range(n):
             if lines[i][j] == 0:
-                res = dfs(i, j)
-                print(i, j, res)
-                result += res
-
+                result += dfs(i, j)
     print(result)
 
 
