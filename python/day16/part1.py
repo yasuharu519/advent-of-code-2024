9052657ade43f98485680ea7f3d9fe5f3150ae8f
Yasuharu Sawada <yasuharu519@gmail.com>
2025-01-06 00:48:39 +0900

    Refactor day16

--- python/day16/part1.py
+++ python/day16/part1.py
@@ -1,19 +1,20 @@
-# coding: utf-8
 import sys
 import heapq
 
 def main():
-    input_map = [list(line.strip()) for line in sys.stdin.readlines()]
-    m = len(input_map)
-    n = len(input_map[0])
+    grid = [list(line.strip()) for line in sys.stdin.readlines()]
+    m = len(grid)
+    n = len(grid[0])
+
     dp = [[[float("inf")] * 4 for _ in range(n)] for _ in range(m)]
 
+    # スタート地点を検索
     sx, sy = (0, 0)
     for i in range(m):
         for j in range(n):
-            if input_map[i][j] == "S":
+            if grid[i][j] == "S":
                 sx, sy = i, j
-                input_map[i][j] = "."
+                grid[i][j] = "."
     
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     
@@ -21,7 +22,7 @@ def main():
     heap = [(0, sx, sy, 1)]
     while heap:
         cost, x, y, d= heapq.heappop(heap)
-        if input_map[x][y] == "E":
+        if grid[x][y] == "E":
             print(cost)
             return
         if dp[x][y][d] <= cost:
@@ -31,15 +32,15 @@ def main():
         # 一歩進む
         dx, dy = directions[d]
         nx, ny = x + dx, y + dy
-        if 0 <= nx < m and 0 <= ny < n and input_map[nx][ny] != "#":
+        if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != "#":
             heapq.heappush(heap, (cost + 1, nx, ny, d))
+
         # 回転
         heapq.heappush(heap, (cost + 1000, x, y, (d + 1) % 4))
         heapq.heappush(heap, (cost + 1000, x, y, (d - 1) % 4))
     
     print(-1)
     return
-
 
 if __name__ == "__main__":
     main()
--- python/day16/part2_aster.py
+++ python/day16/part2_aster.py
@@ -23,6 +23,9 @@ def print_grid_and_path(grid, cell_on_best_routes):
     time.sleep(1)
     return
 
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
 def main():
     grid = [list(line.strip()) for line in sys.stdin.readlines()]
     m = len(grid)
@@ -31,49 +34,61 @@ def main():
     dp = [[[float("inf")] * 4 for _ in range(n)] for _ in range(m)]
 
     sx, sy = (0, 0)
+    ex, ey = (0, 0)
     for i in range(m):
         for j in range(n):
             if grid[i][j] == "S":
                 sx, sy = i, j
                 grid[i][j] = "."
+            elif grid[i][j] == "E":
+                ex, ey = i, j
     
     # N, E, S, W
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     
-    heap = [(0, sx, sy, 1, set([(sx, sy)]))]
+    # かかった
+    heap = [(0 + manhattan_distance(sx, sy, ex, ey), 0, sx, sy, 1, set([(sx, sy)]))]
     best_score = float("inf")
     cell_on_best_routes = set()
 
     steps = 0
     while heap:
-        cost, x, y, d, path = heapq.heappop(heap)
-        # print(f"cost: {cost}, x: {x}, y: {y}, d: {d}, remaining: {len(heap)}")
+        f, g, x, y, d, path = heapq.heappop(heap)
         steps += 1
         if steps % 5000 == 0:
             print_grid_and_path(grid, path)
 
         if grid[x][y] == "E":
             if best_score == float("inf"):
-                best_score = cost
+                best_score = g
                 cell_on_best_routes |= path
-            elif best_score == cost:
+            elif best_score == g:
                 cell_on_best_routes |= path
             else:
-                break
-        if dp[x][y][d] < cost:
+                if g > best_score:
+                    break
+        if dp[x][y][d] < g:
             continue
-        dp[x][y][d] = cost
+        dp[x][y][d] = g
 
         # 一歩進む
         dx, dy = directions[d]
         nx, ny = x + dx, y + dy
         if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] != "#":
-            heapq.heappush(heap, (cost + 1, nx, ny, d, path | set([(nx, ny)])))
+            new_g = g + 1
+            # if new_g <= dp[nx][ny][d]:
+            #     dp[nx][ny][d] = new_g
+            new_h = manhattan_distance(nx, ny, ex, ey)
+            heapq.heappush(heap, (new_g + new_h, new_g, nx, ny, d, path | set([(nx, ny)])))
 
         # 回転
-        heapq.heappush(heap, (cost + 1000, x, y, (d + 1) % 4, path))
-        heapq.heappush(heap, (cost + 1000, x, y, (d - 1) % 4, path))
-    
+        for rorate in [-1, 1]:
+            nd = (d + rorate) % 4
+            new_g = g + 1000
+            # if new_g <= dp[x][y][nd]:
+            #     dp[x][y][nd] = new_g
+            new_h = manhattan_distance(x, y, ex, ey)
+            heapq.heappush(heap, (new_g + new_h, new_g, x, y, nd, path))
 
     print_grid_and_path(grid, cell_on_best_routes)
     print(f"Score: {len(cell_on_best_routes)}")
