06c4b32e698b548366fc92f367a82de041997808
Yasuharu Sawada <yasuharu519@gmail.com>
2025-01-22 00:41:14 +0900

    Update day18

--- python/day18/part1.py
+++ python/day18/part1.py
@@ -1,8 +1,21 @@
 import sys
 from collections import deque
+from typing import List
+
+RED = "\033[31m"
+RESET = "\033[0m"
 
 MAP_W = 71
 MAP_H = 71
+
+def print_grid(dp: List[List[str]]):
+    for row in dp:
+        for c in row:
+            if c == "o":
+                print(f"{RED}o{RESET}", end="")
+            else:
+                print(c, end="")
+        print()
 
 def main():
     lines = sys.stdin.readlines()
@@ -13,17 +26,17 @@ def main():
         y, x = map(int, line.strip().split(","))
         dp[x][y] = "#"
 
-    for row in dp:
-        print("".join(row))
-
     current = (0, 0)
     queue = deque()
-    queue.append((0, current))
+    queue.append((0, current, [(0, 0)]))
     passed = set()
-    # heap = [(0, current)]
     while queue:
-        cost, (x, y) = queue.popleft()
+        cost, (x, y), path = queue.popleft()
         if (x, y) == (MAP_W - 1, MAP_H - 1):
+            # update path on grid
+            for x, y in path:
+                dp[x][y] = "o"
+            print_grid(dp)
             print(cost)
             return
         if (x, y) in passed:
@@ -33,7 +46,7 @@ def main():
         for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             nx, ny = x + dx, y + dy
             if 0 <= nx < MAP_H and 0 <= ny < MAP_W and (nx, ny) not in passed and dp[nx][ny] == ".":
-                queue.append((cost + 1, (nx, ny)))
+                queue.append((cost + 1, (nx, ny), path + [(nx, ny)]))
     print("No path found")
         
     
